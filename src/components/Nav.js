4c36c52b8d04c66476e9e88539bd94445c6cce25
PMKN <shameman@naver.com>
2023-11-21 22:29:36 +0900

    Initial Commit

---
+++ src/components/Nav.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+import "./Nav.css"
+
+export default function Nav() {
+    const [show, setShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if(window.scrollY > 50) {
+                setShow(true);
+            } else {
+                setShow(false);
+            }
+        });
+
+        return () => {
+            window.removeEventListener("scroll", () => {});
+        };
+    });
+
+  return (
+    <nav className={`nav ${show && "nav__black"}`}>
+        <img
+            alt='Netflix logo'
+            src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/440px-Netflix_2015_logo.svg.png'
+            className='nav__logo'
+            onClick={() => window.location.reload()}
+        />
+
+        <img
+            alt='User logged'
+            src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117'
+            className='nav__avatar'
+        />
+    </nav>
+  )
+}
